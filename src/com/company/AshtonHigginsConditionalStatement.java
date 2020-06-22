61ddcf875f58b15f184e41018b4a0e031adc19ad
Ashton <higginsa22@greenhill.org>
2020-06-22 09:03:39 -0500

    This update 6/15/2020

--- src/com/company/AshtonHigginsConditionalStatement.java
+++ src/com/company/AshtonHigginsConditionalStatement.java
@@ -1,3 +1,19 @@
+/* First I will import the scanner
+Main
+I will set up the random variable
+And make a do loop
+	Put the random variable into an integer and limit it to be between 1 and 137
+	Print “Your number is: “ along with their number
+	Input the statement of weird or not weird from the solve method
+	Then I will ask them if they want to go again
+	It will repeat the loop if they type anything with the letter ‘y’
+Then I will set up the solve method
+I will make an if statement if the number is odd
+	Print weird
+Then three else if statements if the number is even
+	Each will have different ranges in value
+	Two will print not weird, and one will print weird
+*/
 package com.company;
 import java.util.*;
 public class AshtonHigginsConditionalStatement {
