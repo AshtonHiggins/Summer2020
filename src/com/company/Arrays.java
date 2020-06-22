f81c4d98b0d89e6b2d31d5d995d81771aa9b78ba
Ashton <higginsa22@greenhill.org>
2020-06-22 16:21:33 -0500

    This update 6/15/2020

---
+++ src/com/company/Arrays.java
@@ -0,0 +1,38 @@
+/*Import scanner
+Ask the user for how many days they want to measure (between 2 and 100)
+	Add 1 to number when we make it a variable (might need to subtract one and then add 2)
+Set up a random number variable (between 25 and 110) to 86+25
+Create a loop that repeats however many times the user input variable is
+Then we add all outputs together and divide by user input variable to get the average
+Print each day’s high temp as a line
+	For every day
+Print the average temp of (user input variable) days
+NEW IDEA FOR PSEUDOCODE:
+Set up an array statement (Temperature at Day X: Rand)
+Then create a variable for X that starts at 1 and increases +1 every time a line is printed
+Replace the char at Rand with a new random number every time the line is printed
+Create this all in a for loop so that it prints however many times the user asks for in the do loop
+*/
+package com.company;
+import java.util.*;
+public class Arrays {
+    public static void main(String[] args) {
+        Scanner Keyboard = new Scanner(System.in);
+        Random rand= new Random();
+        int days;
+        do {
+            System.out.print("How many days temperature (between 2 and 100)? Your answer: ");
+            days = Keyboard.nextInt();
+        } while (days <= 2 || days >= 100);
+        int [] temp=new int[days];
+        int total=0;
+        for (int x=0; x<days; x++){
+            temp[x] = rand.nextInt(85) + 25;
+            System.out.print("On day "+x+1+" the high temperature was: "+rand);
+            total=total+temp[x];
+        }
+        double average= (double) total/days;
+        for()
+    }
+}
+
---
+++ src/com/company/AshtonHigginsStringChallenge.java
@@ -0,0 +1,56 @@
+/*
+Set up a keyboard for them to type
+
+Main Method:
+Ask how many words they will give us (as integer T)
+	Limit it to be between 1-10
+Create a for loop controlled by the value of T
+	Ask the user for a word
+	Find the length of the word
+Declare the string= call method(new word)
+Print the new word that was sent to me
+
+Return Method:
+String even letter
+	For loop (control even=+2) will start at index 0
+	Evenletter=
+String odd letter
+	For loop (odd index) will start at index 1
+	oddletter=
+Return evenletter+” “+oddletter
+ */
+package com.company;
+import java.util.*;
+public class AshtonHigginsStringChallenge {
+    public static void main(String[] args) {
+       int LenString;
+       Scanner Keyboard= new Scanner(System.in);
+       do{
+           System.out.println("How many words would you like to type? Your answer: ");
+           LenString= Keyboard.nextInt();
+       }while (LenString<1||LenString>10);
+       for (int loop=1; loop<=LenString; loop++){
+           System.out.println("Enter a word: ");
+           String inputword= Keyboard.next();
+           if(inputword.length()>=2){
+               String receiveword= scramble(inputword);
+               System.out.println(receiveword);
+           }
+       }
+
+    }
+    public static String scramble(String inputword){
+        StringBuilder evenstring= new StringBuilder();
+        StringBuilder oddstring= new StringBuilder();
+        int stringlength= inputword.length();
+        for (int evenchar= 0; evenchar<=stringlength-1; evenchar=evenchar+2){
+            char output= inputword.charAt(evenchar);
+            evenstring.append(output);
+        }
+        for (int oddchar=1; oddchar<= stringlength-1; oddchar= oddchar+2){
+            char output=inputword.charAt(oddchar);
+            oddstring.append(output);
+        }
+        return evenstring+" "+oddstring;
+    }
+}
